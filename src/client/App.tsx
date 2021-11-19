b2b62afa7517839f92dccd3df99b67f739531aed
Juwan Robinson <juwanrobinson@Sharis-Air.attlocal.net>
2021-11-19 02:35:56 -0600

    New

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,63 +2,68 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 
 /* HOOK REACT EXAMPLE */
-const App = (props: AppProps) => {
-	const [greeting, setGreeting] = useState<string>('');
+// const App = (props: AppProps) => {
+// 	const [greeting, setGreeting] = useState<string>('');
 
-	useEffect(() => {
-		async function getGreeting() {
-			try {
-				const res = await fetch('/api/hello');
-				const greeting = await res.json();
-				setGreeting(greeting);
-			} catch (error) {
-				console.log(error);
-			}
-		}
-		getGreeting();
-	}, []);
+// 	useEffect(() => {
+// 		async function getGreeting() {
+// 			try {
+// 				const res = await fetch('/api/hello');
+// 				const greeting = await res.json();
+// 				setGreeting(greeting);
+// 			} catch (error) {
+// 				console.log(error);
+// 			}
+// 		}
+// 		getGreeting();
+// 	}, []);
 
-	return (
-		<main className="container my-5">
-			<h1 className="text-primary text-center">Hello {greeting}!</h1>
-		</main>
-	);
-};
+// 	return (
+// 		<main className="container my-5">
+// 			<h1 className="text-primary text-center">Hello {greeting}!</h1>
+// 		</main>
+// 	);
+// };
 
 interface AppProps {}
 
 /* CLASS REACT EXAMPLE */
-// class App extends React.Component<IAppProps, IAppState> {
-// 	constructor(props: IAppProps) {
-// 		super(props);
-// 		this.state = {
-// 			name: null
-// 		};
-// 	}
+class App extends React.Component<IAppProps, IAppState> {
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+			blogs: []
+		};
+	}
 
-// 	async componentDidMount() {
-// 		try {
-// 			let r = await fetch('/api/hello');
-// 			let name = await r.json();
-// 			this.setState({ name });
-// 		} catch (error) {
-// 			console.log(error);
-// 		}
-// 	}
+	async componentDidMount() {
+		try {
+			let r = await fetch('/api/blogs');
+			let blogs = await r.json();
+			this.setState({ blogs });
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-// 	render() {
-// 		return (
-// 			<main className="container my-5">
-// 				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-// 			</main>
-// 		);
-// 	}
-// }
+	render() {
+		return (
+			<main className="container my-5">
+				<h1 className="text-primary text-center">My Blog!</h1>
+				<ul className="list group">
+					{this.state.blogs.map(blog => {
+						return <li className="list-group-items">{blog.title}</li>
+					})}
+				</ul>
+			</main>
+		);
+	}
+}
 
-// export interface IAppProps {}
+export interface IAppProps {}
 
-// export interface IAppState {
-// 	name: string;
-// }
+export interface IAppState {
+	blogs: Array<{title: string, body: string}>;
+}
 
 export default App;
---
+++ src/server/config/index.ts
@@ -0,0 +1,10 @@
+export default {
+
+    mysql: {
+        host: 'localhost',
+        port: 3306,
+        user: 'blogapp',
+        database: 'blogs',
+        password: 'password'
+    }
+}
